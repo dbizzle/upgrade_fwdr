cc0abc718a86ae59b4df3664a0e1abe7f67cf6b0
dbizzle <32844769+dbizzle@users.noreply.github.com>
2017-10-18 08:38:58 -0400

    Add files via upload

--- bin/launcher.sh
+++ bin/launcher.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#exec > >(tee -i /opt/splunkforwarder/etc/apps/upgrade_linux_uf/bin/script.log) 2>&1
+
 echo upgrade_linux_uf wrapper.sh Starting >&2
-( /opt/splunkforwarder/etc/apps/upgrade_linux_uf/bin/upgrade.sh & )
+
+( /opt/splunkforwarder/etc/apps/upgrade_linux_uf/bin/upgrade.sh )& 
